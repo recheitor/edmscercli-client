df629723c22732f22cab78c7d9d845aa26c2ef5a
recheitor <arc19881@gmail.com>
2024-06-27 21:19:12 +0200

    spinner

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,16 +1,19 @@
 import { useState, useEffect, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Card, CardContent, Typography, Avatar, Button } from '@mui/material';
+import { Box, Card, CardContent, Typography, Avatar, Button, CircularProgress } from '@mui/material';
 import { formatDate } from '../../utils/dateFormatter';
 import { Employee, EmployeesService, GetAllEmployeesResponse } from '../../api/EmployeesService';
 
 function AdminPage(): ReactElement {
   const [employees, setEmployees] = useState<Employee[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true); // Start loading
     EmployeesService.getAllEmployees()
       .then(({ data }: GetAllEmployeesResponse) => {
         setEmployees(data);
+        setLoading(false); // Stop loading after data is received
       })
       .catch((err: unknown) => {
         if (err instanceof Error) {
@@ -18,33 +21,40 @@ function AdminPage(): ReactElement {
         } else {
           console.error('Unexpected error:', err);
         }
+        setLoading(false); // Stop loading even if there is an error
       });
   }, []);
 
   return (
     <Box>
       <Typography variant="h4" gutterBottom>Current Employees</Typography>
-      <Box>
-        {employees.map(employee => (
-          <Link to={`/admin/employee/${employee._id}`} key={employee._id} style={{ display: 'block' }}>
-            <Card sx={{ display: 'flex', marginBottom: 2, boxShadow: 3, width: '100%', padding: 1, transition: '0.3s', '&:hover': { boxShadow: 6, backgroundColor: 'primary.light', cursor: 'pointer' } }}>
-              <Avatar sx={{ width: 50, height: 50, margin: 2 }} src="/static/images/avatar/1.jpg" alt={employee.name} />
-              <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                <CardContent>
-                  <Typography variant="h6" component="div" color="text.primary">
-                    {employee.name.toUpperCase()}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">Position: {employee.position}</Typography>
-                  <Typography variant="body2" color="text.secondary">Email: {employee.email}</Typography>
-                  <Typography variant="body2" color="text.secondary">Salary: {employee.salary}</Typography>
-                  <Typography variant="body2" color="text.secondary">Created: {formatDate(employee.createdAt)}</Typography>
-                  <Typography variant="body2" color="text.secondary">Updated: {formatDate(employee.updatedAt)}</Typography>
-                </CardContent>
-              </Box>
-            </Card>
-          </Link>
-        ))}
-      </Box>
+      {loading ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box>
+          {employees.map(employee => (
+            <Link to={`/admin/employee/${employee._id}`} key={employee._id} style={{ display: 'block' }}>
+              <Card sx={{ display: 'flex', marginBottom: 2, boxShadow: 3, width: '100%', padding: 1, transition: '0.3s', '&:hover': { boxShadow: 6, backgroundColor: 'primary.light', cursor: 'pointer' } }}>
+                <Avatar sx={{ width: 50, height: 50, margin: 2 }} src="/static/images/avatar/1.jpg" alt={employee.name} />
+                <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                  <CardContent>
+                    <Typography variant="h6" component="div" color="text.primary">
+                      {employee.name.toUpperCase()}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">Position: {employee.position}</Typography>
+                    <Typography variant="body2" color="text.secondary">Email: {employee.email}</Typography>
+                    <Typography variant="body2" color="text.secondary">Salary: {employee.salary}</Typography>
+                    <Typography variant="body2" color="text.secondary">Created: {formatDate(employee.createdAt)}</Typography>
+                    <Typography variant="body2" color="text.secondary">Updated: {formatDate(employee.updatedAt)}</Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Link>
+          ))}
+        </Box>
+      )}
       <Link to="employee/new">
         <Button variant="outlined" size="large" sx={{ margin: '2vw' }}>Add Employee</Button>
       </Link>
